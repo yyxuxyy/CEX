f323c26331f39844888ea0cc396b6325cf5c79f1
yyxuxyy <yyxuxyy@hotmail.com>
2019-06-08 15:45:09 +0800

    ex1

---
+++ ex1/count.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+
+    printf("oov\n");
+
+    system("pause");
+
+    return 0;
+}
---
+++ ex1/wc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("hello world \c");
+
+    printf("mimia\c\c\c\c\c\cc");
+
+    system("pause");
+}
